c402b85554c15e85252ec36ac72f3e844a716f1a
RCA <69407614+RafaelCAguiar@users.noreply.github.com>
2021-01-28 14:58:30 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -23,17 +28,47 @@ export const QuizContainer = styled.div`
   }
   `;
 
-   
 export default function Home (){
+  const router = useRouter();
+  const [name, setName] = React.useState ('');
+  console.log('retorno do useState' , name, setName);
+
   return (
     <QuizBackground backgroungImage = {db.bg}>
+      <Head>
+        <title>Filosofia Quiz - Modelo Base</title>
+        <meta property="og:image" content={"https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1"}/>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
             <Widget.Header>
             <h1>Filosofia Quiz</h1>
             </Widget.Header>
             <Widget.Content>
-               <p>loren ipsum dolor sit amet...</p>
+              <form onSubmit={function (infosDoEvento){
+                infosDoEvento.preventDefault();
+                                
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submissão por meio do react');
+                
+                // router manda para a próxima página
+              }}
+              >
+              <input
+               onChange={function (infosDoEvento) {
+                console.log(infosDoEvento.target.value); 
+                // State (A "foto" da tela - o momento)
+                // name = infosDoEvento.target.value;
+                setName(infosDoEvento.target.value);
+               }}
+               placeholder="Diz ai seu nome" 
+              />
+              <button type="submit" disable={name.length ===0}>
+                Jogar 
+                {name}
+              </button>
+              </form>
           </Widget.Content>          
         </Widget>
 
